8efd2034fd23c654db935de8909f8d002e34eb48
Elena <elena.smyshlyaeva01@gmail.com>
2021-12-21 18:18:14 +0300

    controller

---
+++ demoSpringSecurity/src/main/java/com/example/demospringsecurity/rest/DeveloperRestControllerV1.java
@@ -0,0 +1,42 @@
+package com.example.demospringsecurity.rest;
+
+import com.example.demospringsecurity.domain.Developer;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/api/v1/developers")
+public class DeveloperRestControllerV1 {
+    private List<Developer> DEVELOPERS = Stream.of(
+            new Developer(1L,"Ivan", "Ivanov"),
+            new Developer(2L, "Petr", "Petrov"),
+            new Developer(3L, "Alexey", "Alexiev")
+    ).collect(Collectors.toList());
+
+    @GetMapping
+    public List<Developer> getAll() {
+        return DEVELOPERS;
+    }
+
+    @GetMapping("/{id}")
+    public Developer getById(@PathVariable Long id) {
+        return DEVELOPERS.stream()
+                .filter(developer -> developer.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @PostMapping
+    public Developer create(@RequestBody Developer developer) {
+        this.DEVELOPERS.add(developer);
+        return developer;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Long id) {
+        DEVELOPERS.removeIf(developer -> developer.getId().equals(id));
+    }
+}
