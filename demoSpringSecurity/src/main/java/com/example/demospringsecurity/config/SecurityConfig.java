8ae2a06459773b94b20237dda59c15a44a5b74d9
Elena <elena.smyshlyaeva01@gmail.com>
2021-12-26 09:44:33 +0300

    JWT

--- demoSpringSecurity/src/main/java/com/example/demospringsecurity/config/SecurityConfig.java
+++ demoSpringSecurity/src/main/java/com/example/demospringsecurity/config/SecurityConfig.java
@@ -1,15 +1,18 @@
 package com.example.demospringsecurity.config;
 
+import com.example.demospringsecurity.security.JwtConfigurer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,58 +21,38 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final UserDetailsService userDetailsService;
+    private final JwtConfigurer jwtConfigurer;
 
-    @Autowired
-    public SecurityConfig(@Qualifier("userDetailsServiceImpl") UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
+    public SecurityConfig(JwtConfigurer jwtConfigurer) {
+        this.jwtConfigurer = jwtConfigurer;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
 
                 .authorizeRequests()    //авторизация запроса (определение его прав доступа)
                 .antMatchers("/")   //какие паттерны урлов имеется доступ
                 .permitAll()   //доступ к главной странице ("/") есть у всех
 
+                .antMatchers("/api/v1/auth/login").permitAll()
+
                 //любой запрос должен быть аутентифицирован (с помощью формы)
-                .anyRequest().authenticated().and()
-                .formLogin()
-                .loginPage("/auth/login") //ссылка на страницу
-                .permitAll() //все имеют доступ
-                .defaultSuccessUrl("/auth/success") //ссылка на страницу, если вход успешен
-
-                //когда logout отдается через метод GET - это не безопасно, поэтому изменим на POST
+                .anyRequest().authenticated()
                 .and()
-                .logout()
+                .apply(jwtConfigurer); //теперь аутентификация пользователя происходит на основании переданной конфигурации
 
-                //соединяем запрос с логаутом: указываем ссылку и метод, по которому он будет происходить
-                .logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout", "POST"))
-
-                //убираем за собой
-                .invalidateHttpSession(true)
-                .clearAuthentication(true)
-                .deleteCookies("JSESSIONID")
-                .logoutSuccessUrl("/auth/login"); //на какую страницу перенаправляет после логаута
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(daoAuthenticationProvider());
-    }
-
-    //бин для получения аутентификации
     @Bean
-    protected DaoAuthenticationProvider daoAuthenticationProvider() {
-        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
-        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
-        return daoAuthenticationProvider;
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     //bean for encrypt password
